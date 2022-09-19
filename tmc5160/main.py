1cbba6ad6628f71a3271312be1848c9b1142c79c
Augustin Bielefeld <Augustin.Bielefeld@maximintegrated.com>
2022-09-19 15:28:51 +0200

    main: Create main and call configurator from it

---
+++ tmc5160/main.py
@@ -0,0 +1,9 @@
+import pytrinamic
+from helpers.config import Tmc5160Config
+from pytrinamic.connections import ConnectionManager
+from pytrinamic.evalboards import TMC5160_eval
+
+pytrinamic.show_info()
+with ConnectionManager().connect() as my_interface:
+    configurator = Tmc5160Config(my_interface, encoder_tick_per_turn=10000, steps_per_turn=200)
+    configurator.config_encoder()
