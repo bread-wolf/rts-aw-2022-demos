101d58697cc1e7e95ab29cb34f2249592b59988a
Augustin Bielefeld <Augustin.Bielefeld@maximintegrated.com>
2022-09-19 15:59:58 +0200

    main: Call ramper config function, request speed of 1 RPS

--- tmc5160/main.py
+++ tmc5160/main.py
@@ -7,3 +7,4 @@ pytrinamic.show_info()
 with ConnectionManager().connect() as my_interface:
     configurator = Tmc5160Config(my_interface, encoder_tick_per_turn=10000, steps_per_turn=200)
     configurator.config_encoder()
+    configurator.config_ramper(vmax=1.0)
